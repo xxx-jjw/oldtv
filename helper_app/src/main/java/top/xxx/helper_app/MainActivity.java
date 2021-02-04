b5beeadf9ce5cae9189c1af87ce589103f14bec8
x <2822858608@qq.com>
2021-02-04 13:15:58 +0800

    大致确定各模块功能，功能描述在各个模块的readme中。

---
+++ helper_app/src/main/java/top/xxx/helper_app/MainActivity.java
@@ -0,0 +1,16 @@
+package top.xxx.helper_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.helper_app.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ remote_control_app/src/main/java/top/xxx/remote_control_app/MainActivity.java
@@ -0,0 +1,16 @@
+package top.xxx.remote_control_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.oldtv.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
