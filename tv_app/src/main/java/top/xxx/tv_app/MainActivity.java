7f88e0bb55c6ec34fe609ee54578e8cdd739cdac
x <2822858608@qq.com>
2021-02-14 18:23:01 +0800

    (1).tv_app模块完成单频道播放。 (2).tv_app模块播放器activity设置为全屏。此处全屏方法可能留坑，是在清单文件中关闭了标题栏，在onCreate中隐藏了状态栏。

--- tv_app/src/main/java/top/xxx/tv_app/MainActivity.java
+++ tv_app/src/main/java/top/xxx/tv_app/MainActivity.java
@@ -2,13 +2,35 @@ package top.xxx.tv_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.pili.pldroid.player.widget.PLVideoView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private PLVideoView plVideoview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        无标题栏：
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+//        全屏：无状态栏
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+//        Uri uri = Uri.parse("http://ivi.bupt.edu.cn/hls/cctv1hd.m3u8");
+        Uri uri = Uri.parse("http://cctvalih5ca.v.myalicdn.com/live/cctv1_2/index.m3u8");
+
+        plVideoview = (PLVideoView) findViewById(R.id.plVideoView);
+        plVideoview.setVideoURI(uri);
+
+        // 设置全屏播放, 不能取消标题栏和状态栏
+        plVideoview.setDisplayAspectRatio(PLVideoView.ASPECT_RATIO_PAVED_PARENT);
+        plVideoview.start();
+//        android:theme="@style/Theme.OldTV"
     }
 }
