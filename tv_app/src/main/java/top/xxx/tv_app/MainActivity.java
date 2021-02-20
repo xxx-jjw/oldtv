2705ac61a11277b29f552c3f1fe56e6277b86f45
x <2822858608@qq.com>
2021-02-20 23:51:32 +0800

    tv端添加遥控消息接收服务的代码。若要处理更多按键(包括遥控按键)消息，在MainActivity的onKeyDown方法中添加相应逻辑即可。

--- tv_app/src/main/java/top/xxx/tv_app/MainActivity.java
+++ tv_app/src/main/java/top/xxx/tv_app/MainActivity.java
@@ -11,9 +11,26 @@ import android.view.WindowManager;
 
 import com.pili.pldroid.player.widget.PLVideoView;
 
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
+import top.xxx.tv_app.util.KeyUtil;
+
 public class MainActivity extends AppCompatActivity {
 
     private PLVideoView plVideoview;
+    Thread remoteControlServerThread = new Thread(){
+        @Override
+        public void run() {
+            super.run();
+            super.run();
+            try {
+                listenPhoneKeyMessage();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +38,8 @@ public class MainActivity extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        remoteControlServerThread.start();
 
         plVideoview = (PLVideoView) findViewById(R.id.plVideoView);
 
@@ -32,6 +51,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        remoteControlServerThread.stop();
+    }
+
+    @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         Log.e("1", ""+keyCode);
         // keycode      action
@@ -39,5 +64,19 @@ public class MainActivity extends AppCompatActivity {
         // 25           volume-
         // 19,20,21,22  up,down,left,right
         return super.onKeyDown(keyCode, event);
+    }
+
+    private void listenPhoneKeyMessage() throws Exception {
+        int selfPort = 2106;
+
+        DatagramSocket socket = new DatagramSocket(selfPort);
+        byte[] receiveData = new byte[1024];
+        DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+
+        while (true) {
+            socket.receive(receivePacket);
+            int keyCode = Integer.parseInt(new String(receiveData, 0, receivePacket.getLength()));
+            KeyUtil.sendKeyCode(keyCode);
+        }
     }
 }
