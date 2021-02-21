1e88f9e5947e9a97b1c20bd9980dd23fd9de1deb
x <2822858608@qq.com>
2021-02-21 18:46:54 +0800

    tv模块：(1)完成频道+、频道-的处理逻辑。(2)启动时进入的频道由文件决定，文件内容每次切换频道时修改(暂未实现)。

--- tv_app/src/main/java/top/xxx/tv_app/MainActivity.java
+++ tv_app/src/main/java/top/xxx/tv_app/MainActivity.java
@@ -12,9 +12,16 @@ import android.widget.Toast;
 
 import com.pili.pldroid.player.widget.PLVideoView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.util.ArrayList;
+import java.util.List;
 
+import top.xxx.tv_app.pojo.Channel;
 import top.xxx.tv_app.util.KeyUtil;
 
 public class MainActivity extends AppCompatActivity {
@@ -24,7 +31,6 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void run() {
             super.run();
-            super.run();
             try {
                 listenPhoneKeyMessage();
             } catch (Exception e) {
@@ -32,6 +38,8 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     };
+    List<Channel> channelList = new ArrayList<Channel>();
+    int curChannelNum = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,11 +53,17 @@ public class MainActivity extends AppCompatActivity {
 
         plVideoview = (PLVideoView) findViewById(R.id.plVideoView);
 
-        Uri uri = Uri.parse("http://cctvalih5ca.v.myalicdn.com/live/cctv1_2/index.m3u8");
-        plVideoview.setVideoURI(uri);
         // 设置全屏播放, 不能取消标题栏和状态栏
-//        plVideoview.setDisplayAspectRatio(PLVideoView.ASPECT_RATIO_PAVED_PARENT);
-        plVideoview.start();
+//        plVideoview.setDisplayAspectRatio(PLVideoView.ASPECT_RATIO_PAVED_PARENT);;
+
+        channelList.add(new Channel("点播台", ""));
+        channelList.add(new Channel("戏曲台", ""));
+        channelList.add(new Channel("电视剧台", ""));
+        channelList.add(new Channel("电影台", ""));
+
+        readChannelByFileAndAddChannelToList();
+        readCurChannelNumByFile();
+        changeChannel(curChannelNum);
     }
 
     @Override
@@ -70,9 +84,13 @@ public class MainActivity extends AppCompatActivity {
         switch (keyCode){
             case KeyEvent.KEYCODE_DPAD_UP://频道+
                 Toast.makeText(MainActivity.this, "频道+", Toast.LENGTH_SHORT).show();
+                curChannelNum = (curChannelNum+1) % channelList.size();
+                changeChannel(curChannelNum);
                 break;
             case KeyEvent.KEYCODE_DPAD_DOWN://频道-
                 Toast.makeText(MainActivity.this, "频道-", Toast.LENGTH_SHORT).show();
+                curChannelNum = (curChannelNum -1 + channelList.size()) % channelList.size();
+                changeChannel(curChannelNum);
                 break;
         }
         return super.onKeyDown(keyCode, event);
@@ -90,5 +108,45 @@ public class MainActivity extends AppCompatActivity {
             int keyCode = Integer.parseInt(new String(receiveData, 0, receivePacket.getLength()));
             KeyUtil.sendKeyCode(keyCode);
         }
+    }
+
+    private void readChannelByFileAndAddChannelToList(){
+        try {
+            InputStream is = getResources().getAssets().open("channel.data");
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            String str = null;
+            while ((str = br.readLine()) != null) {
+                //            Toast.makeText(this, str, Toast.LENGTH_SHORT).show();
+                String[] temp = str.split(" ");
+                channelList.add(new Channel(temp[0], temp[1]));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void readCurChannelNumByFile(){
+        try {
+            InputStream is = getResources().getAssets().open("cur_channel_num.data");
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            String str = br.readLine();
+            curChannelNum = Integer.parseInt(str);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    private void playWithUriStr(String uriStr){
+        plVideoview.stopPlayback();
+        Uri uri = Uri.parse(uriStr.replace(" ", ""));
+        plVideoview.setVideoURI(uri);
+        plVideoview.start();
+    }
+
+    private void changeChannel(int channelNum){
+        if(channelNum<channelList.size())
+            playWithUriStr(channelList.get(channelNum).getUri());
+        else
+            playWithUriStr(channelList.get(channelList.size() - 1).getUri());
     }
 }
